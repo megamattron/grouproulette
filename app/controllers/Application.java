359720c23b4221e10f6df64384e74c40f15c39ea
Matt Hall <matt@docracy.com>
2012-01-24 20:58:00 -0500

    Basic starter methods.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,28 +1,54 @@
 package controllers;
 
+import models.Message;
+import models.User;
 import play.*;
 import play.modules.redis.Redis;
 import play.mvc.*;
+import services.Data;
+
+import java.util.List;
+import java.util.Set;
 
 public class Application extends Controller {
 
+    private static final String USERNAME = "username";
+
     public static void index() {
         // todo - render login page if no cookie, main page if there is a cookie
+        String username = getUsername();
+        if (username == null) {
+
+        } else {
+            Logger.info("User has already logged in as " + username);
+        }
         String value = Redis.get("blam");
         render(value);
     }
 
+    private static String getUsername() {
+        return session.get(USERNAME);
+    }
+
     public static void login(String username) {
-        // todo - store some stuff, render main page, store cookie
+        session.put(USERNAME, username);
+        Logger.info("You are now user " + username);
         String groupId = "the group";
+
+        Data.addUserToGroup(username, groupId);
         render(username, groupId);
     }
 
-    public static void getMessages(String groupId) {
-        // todo - get all messages, dump to json
-        renderJSON(null);
+    public static void getUsers(String groupId) {
+//        List<User> usersForGroup = Data.getUsersForGroup(groupId);
+//        renderJSON(usersForGroup);
     }
-    
+
+    public static void getMessages(String groupId) {
+        List<Message> messagesForGroup = Data.getMessagesForGroup(groupId);
+        renderJSON(messagesForGroup);
+    }
+
     public static void postMessage(String groupId, String text) {
         // todo - get username from cookie (or should it be a request param?), store
         renderJSON(null);
@@ -31,5 +57,25 @@ public class Application extends Controller {
     public static void poll(String groupId, String lastMessageId) {
         // todo - get the group messages and see if there is anything after the message ID. If so, then JSON it.
         renderJSON(null);
+    }
+
+    public static void makeTestData() {
+        User user1 = new User("user1", "Billy Blogpost");
+        User user2 = new User("user1", "Jimmy Donut");
+        Data.addUser(user1);
+        Data.addUser(user2);
+        String groupId = Data.addGroup();
+        Data.addUserToGroup(user1.id, groupId);
+        Data.addUserToGroup(user2.id, groupId);
+        Data.postMessage(user1.id, groupId, "Billy Blogpost here with another weblog entry, thanks.");
+        Data.postMessage(user2.id, groupId, "Couldn't be happier over here, all the best, Jimmy Donut.");
+        final Set<String> allGroups = Data.getAllGroups();
+        for (String group : allGroups) {
+            System.out.println("Group: " + group);
+        }
+        final List<Message> messagesForGroup = Data.getMessagesForGroup(groupId);
+        for (Message message : messagesForGroup) {
+            System.out.println("Message: " + message);
+        }
     }
 }
